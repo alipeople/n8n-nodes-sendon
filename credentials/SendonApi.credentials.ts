ceb3459f1273b4cd709c46426547954492d8e518
teho <teho@alipeople.kr>
2025-07-15 15:11:13 +0900

    Added authenticate, test to verify

--- credentials/SendonApi.credentials.ts
+++ credentials/SendonApi.credentials.ts
@@ -1,6 +1,8 @@
 import {
+  IAuthenticateGeneric,
+  ICredentialTestRequest,
   ICredentialType,
-  INodeProperties,
+  INodeProperties
 } from 'n8n-workflow';
 
 export class SendonApi implements ICredentialType {
@@ -24,4 +26,44 @@ export class SendonApi implements ICredentialType {
       default: '',
     },
   ];
+
+  authenticate: IAuthenticateGeneric = {
+    type: 'generic',
+    properties: {
+      headers: {
+        Authorization: '={{"Basic " + Buffer.from($credentials.id + ":" + $credentials.apiKey).toString("base64")}}',
+      },
+    },
+  };
+
+  test: ICredentialTestRequest = {
+    // Note: Sendon service returns 403 Forbidden for non-subscribers. Only valid subscribers will receive 200.
+    request: {
+      method: 'GET',
+      url: 'https://api.sendon.io/v2/messages/sms/groups',
+    },
+    rules: [
+      {
+        type: 'responseCode',
+        properties: {
+          value: 200,
+          message: 'Expected HTTP 200 response when credentials are valid',
+        },
+      },
+      {
+        type: 'responseCode',
+        properties: {
+          value: 401,
+          message: 'Received 401 Unauthorized. Your IP might not be whitelisted for Sendon API access.',
+        },
+      },
+      {
+        type: 'responseCode',
+        properties: {
+          value: 403,
+          message: 'Received 403 Forbidden. You might not be a registered Sendon subscriber.',
+        },
+      },
+    ],
+  };
 }
